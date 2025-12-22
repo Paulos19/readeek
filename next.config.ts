f3d474831e0b93954e89a74da2bbfc8f45a9eb45
Paulos19 <paulohenrique.012araujo@gmail.com>
2025-12-22 02:28:58 -0300

    books

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,15 @@ const nextConfig: NextConfig = {
   },
   typescript: {
     ignoreBuildErrors: true
-  }
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'bzbrxkmhdxvh0b4p.public.blob.vercel-storage.com', // Domínio padrão do Vercel Blob
+      },
+    ],
+  },
 };
 
 export default nextConfig;
