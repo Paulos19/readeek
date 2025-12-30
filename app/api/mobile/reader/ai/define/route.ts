6c21d7ba990679ec3456d50f186116ac82a3b0bc
Paulos19 <paulohenrique.012araujo@gmail.com>
2025-12-30 00:12:53 -0300

    first commit

--- app/api/mobile/reader/ai/define/route.ts
+++ app/api/mobile/reader/ai/define/route.ts
@@ -9,7 +9,7 @@ export async function POST(req: NextRequest) {
 
     if (!text) return NextResponse.json({ error: "Texto inválido" }, { status: 400 });
 
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
     const prompt = `
       Você é um dicionário e professor de literatura conciso.
